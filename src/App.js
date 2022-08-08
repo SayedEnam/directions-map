03085c7b053e6d7d5e3c94b186f5028395f285d5
SayedEnam <syedenamshohag@gmail.com>
2022-08-09 00:41:22 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {  useEffect } from 'react';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3llZGVuYW0iLCJhIjoiY2w2Nnlqc2NvM2Z5bTNjbXBydm9jbjJldyJ9.0a1MGXkwIH-i9gISU1KIUQ';
 
 function App() {
+
+
+useEffect(() => {
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [90.374990, 22.700410],
+    zoom: 13
+    });
+     
+    map.addControl(
+    new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+    }),
+    'top-left'
+    );
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div id='map' className="map-container" />
     </div>
   );
 }
